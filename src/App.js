073b1dee84ec44486335219eea462fb0e01c8800
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-09-23 09:08:55 +0200

    render all information we need to render the differents characters

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      characters: {}
+      characters: []
     }
   }
 
@@ -24,8 +24,22 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <header></header>
-        <main></main>
+        <header className="header">
+          <h1 className="header__title">Ricky and Morty</h1>
+        </header>
+        <main className="main">
+          <ul className="main__list">
+            {this.state.characters.map(character =>
+              <li key={character.id} className="main__list__element">
+                <h2 className="main__list__element__name">{character.name}</h2>
+                <div className="main__list__element__image-container">
+                  <img classname="main__list__element__image" src={character.image} alt={character.name}/>
+                </div>
+                <p className="main__list__element__species">{character.species}</p>
+              </li>
+              )}
+          </ul>
+        </main>
       </div>
     )
   }
