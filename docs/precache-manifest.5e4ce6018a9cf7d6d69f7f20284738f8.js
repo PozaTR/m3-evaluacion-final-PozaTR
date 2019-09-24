05b67c9f088613e450202efc031dce405cc75ab9
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-09-24 08:42:49 +0200

    create docs to publish the project

---
+++ docs/precache-manifest.5e4ce6018a9cf7d6d69f7f20284738f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397d66b12654c432de2992fc3f453d99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e634765182ec73af379",
+    "url": "./static/css/main.7f588a58.chunk.css"
+  },
+  {
+    "revision": "99f7fd0da2365dae9517",
+    "url": "./static/js/2.42c52320.chunk.js"
+  },
+  {
+    "revision": "5e634765182ec73af379",
+    "url": "./static/js/main.beae1e19.chunk.js"
+  },
+  {
+    "revision": "edccfc27b2e08eea20ad",
+    "url": "./static/js/runtime-main.e5244d1e.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty_-_logo.75509f2e.png"
+  }
+]);
